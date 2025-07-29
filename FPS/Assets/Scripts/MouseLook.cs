32238dc4169726368e60148493cb5e2d1543b61e
lastjonicorn <joni.k.lampela@gmail.com>
2025-07-29 15:21:20 +0300

    Terrain details

--- FPS/Assets/Scripts/MouseLook.cs
+++ FPS/Assets/Scripts/MouseLook.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
+    public float smoothTime = 0.05f;
     public Transform playerBody;
+
     private float xRotation = 0f;
+
+    private Vector2 currentMouseDelta;
+    private Vector2 currentMouseDeltaVelocity;
 
     void Start()
     {
@@ -13,14 +18,26 @@ public class MouseLook : MonoBehaviour
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.smoothDeltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.smoothDeltaTime;
+        Vector2 targetMouseDelta = new Vector2(
+            Input.GetAxisRaw("Mouse X"),
+            Input.GetAxisRaw("Mouse Y")
+        );
+
+        // Smooth damp makes movement smooth but responsive
+        currentMouseDelta = Vector2.SmoothDamp(
+            currentMouseDelta,
+            targetMouseDelta,
+            ref currentMouseDeltaVelocity,
+            smoothTime
+        );
+
+        float mouseX = currentMouseDelta.x * mouseSensitivity * Time.deltaTime;
+        float mouseY = currentMouseDelta.y * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
